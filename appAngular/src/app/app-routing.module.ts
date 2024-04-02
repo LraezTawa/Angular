26471646e1eda85f4710e6cff9bc2aa20d78a7f3
LraezTawa <luis.raez@tawa.com.pe>
2024-04-02 12:10:08 -0500

    2.5 - ¿Qué son las rutas hijas?

--- appAngular/src/app/app-routing.module.ts
+++ appAngular/src/app/app-routing.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HomeChildOneComponent } from './home-child-one/home-child-one.component';
+import { HomeChildTwoComponent } from './home-child-two/home-child-two.component';
 const routes: Routes = [
   {
-    path:'auth',
-    loadChildren: () => import('@modules/auth/auth.module').then((m)=>m.AuthModule)
-  },
-  {
-    path:'task',
-    loadChildren: () => import('@modules/task/task.module').then((m)=>m.TaskModule)
+    path:'',
+    component: HomeComponent,
+    children:[
+      {
+        path:'one',
+        component:HomeChildOneComponent
+      },
+      {
+        path:'two',
+        component:HomeChildTwoComponent
+      }
+    ]
   }
+  // {
+  //   path:'auth',
+  //   loadChildren: () => import('@modules/auth/auth.module').then((m)=>m.AuthModule)
+  // },
+  // {
+  //   path:'task',
+  //   loadChildren: () => import('@modules/task/task.module').then((m)=>m.TaskModule)
+  // }
 ];
 
 @NgModule({
