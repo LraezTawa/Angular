3af5600d530de4dc92ccfa7ba0f406d2ffb98069
LraezTawa <luis.raez@tawa.com.pe>
2024-04-02 15:47:06 -0500

    2.7 - Qué son los pipes y cómo generarlos

--- appAngular/src/app/modules/auth/components/login-page/login-page.component.ts
+++ appAngular/src/app/modules/auth/components/login-page/login-page.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { ReplaceSpacePipe } from '@shared/pipes/replace-space.pipe';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
-export class LoginPageComponent {
 
+export class LoginPageComponent {
+  value='hola mundo feo'
+  constructor(private replaceSpace:ReplaceSpacePipe){this.value= replaceSpace.transform(this.value,'ay')}
+
+  
 }
